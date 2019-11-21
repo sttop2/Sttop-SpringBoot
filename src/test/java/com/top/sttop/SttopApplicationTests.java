f8871ecf929600176aec91718bd2ca687e5b55ef
java3 <fengchu@qq.com>
2019-11-21 16:04:52 +0800

    初始化spring boot项目，集成github

---
+++ src/test/java/com/top/sttop/SttopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.top.sttop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SttopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
